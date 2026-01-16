ed91b2ba8dbbc733ebe310bb79bbefb3fecc57cb
JavierMeO <150411748+JavierMeO@users.noreply.github.com>
2026-01-16 14:20:28 -0800

    final?

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pricing: resolve(__dirname, "pages/privacy_politic.html"),
+        contact: resolve(__dirname, "pages/terms_Conditions.html"),
+      },
+    },
   },
-  plugins: [],
-  base: '/'
-}
+});
